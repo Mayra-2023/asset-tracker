9a3d958d92cdd84829e9953fcb02c297b34b3934
Mayra <lulufeliz93@gmail.com>
2026-06-10 08:01:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, url_for, Response
 import os
 import psycopg2
 from datetime import datetime
