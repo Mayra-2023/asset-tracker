1c86b9f9aaa84de69e6fa095ad2cfaf70c5eccc0
Mayra <lulufeliz93@gmail.com>
2026-06-15 10:43:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -615,5 +615,7 @@ def export():
 # RUN
 # =========================
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
